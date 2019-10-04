2767a28da59958c12865826364ad72b23363eb19
afras21 <afras.ali21@gmail.com>
2019-10-04 23:00:03 +0530

    search key using get added!

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
 
+
+
+var fs = require('fs'); 
+var parse = require('csv-parse');
+
+var csvData=[];
+
+
+
+
 app.get('/', (req, res) => {
-    res.send({hi : 'running'});
+    console.log('---',req.query.name)
+    console.log('running...')
+    // res.send('Welcome');
+
+    fs.createReadStream('file.csv')
+    .pipe(parse({delimiter: ','}))
+    .on('data', function(csvrow) {
+        if(csvrow[0] === req.query.name) {
+            console.log(csvrow[1]);
+            res.send('Name: ' + csvrow[0] + '\n' + 'Phone: ' + csvrow[1])
+        }
+        // console.log(csvrow[1]);
+        // csvData.push(csvrow);        
+    })
+    .on('end',function() {
+      console.log('Loaded Successfully!');
+    });
+
 });
+
+// reading csv
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
