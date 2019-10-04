30f0d3cdf070ccabde5976c621b02b83ab12e727
afras21 <afras.ali21@gmail.com>
2019-10-04 23:26:47 +0530

    consoles removed and code aligned

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const app = express();
-
-
-
-var fs = require('fs'); 
+var fs = require('fs');
 var parse = require('csv-parse');
+var csvData = [];
 
-var csvData=[];
-
-
+// for empty page
 app.get('/', (req, res) => {
     res.send('Welcome')
 });
 
+// for search key
 app.get('/contacts', (req, res) => {
-    console.log('---',req.query.name)
-    console.log('running...')
-    // res.send('Welcome');
-
     fs.createReadStream('file.csv')
-    .pipe(parse({delimiter: ','}))
-    .on('data', function(csvrow) {
+        .pipe(parse({ delimiter: ',' }))
+        .on('data', function (csvrow) {
 
-        if(req.query && csvrow[0] === req.query.name) {
-            console.log(csvrow[1]);
-            res.send('Name: ' + csvrow[0] + '\n' + 'Phone: ' + csvrow[1])
-        }
-        // console.log(csvrow[1]);
-        // csvData.push(csvrow);        
-    })
-    .on('end',function() {
-      console.log('Loaded Successfully!');
-    });
+            if (req.query && csvrow[0] === req.query.name) {
+                console.log(csvrow[1]);
+                res.send('Name: ' + csvrow[0] + '\n' + 'Phone: ' + csvrow[1])
+            }
+            csvData.push(csvrow);
+        })
+        .on('end', function () {
+            console.log('Loaded Successfully!');
+        });
 
 });
-
-// reading csv
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
